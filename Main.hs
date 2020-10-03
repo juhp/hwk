c8aa2cbcfc1444e330fbd679110b6e7dad26123c
Jens Petersen <petersen@redhat.com>
2020-10-04 01:36:54 +0800

    major rework to use hint library
    now requires hwk.hs for toList and module Hwk

--- Main.hs
+++ Main.hs
@@ -1,68 +1,61 @@
-{-# LANGUAGE CPP #-}
-
 import Control.Monad.Extra
-import SimpleCmd
-#if MIN_VERSION_simple_cmd(0,2,1)
-  hiding (ifM)
-#endif
+import qualified Data.List as L
+import Data.Maybe
+import Language.Haskell.Interpreter
 import SimpleCmdArgs
 import System.Directory
 import System.FilePath
-import System.IO.Extra
-import System.Process
 
-import Paths_hwk (version)
+import Paths_hwk (getDataDir, version)
 
 main :: IO ()
 main =
   simpleCmdArgs (Just version) "A Haskell awk/sed like tool"
     "Simple shell text processing with Haskell" $
-  runExpr <$> switchWith 'd' "debug" "Output Haskell" <*> optional (strOptionWith 'f' "file" "FILE" "User function include file") <*> strArg "FUNCTION"
+  runExpr <$> switchWith 'a' "" "Output Haskell" <*> optional (strOptionWith 'f' "file" "FILE" "User function include file") <*> strArg "FUNCTION" <*> many (strArg "FILE...")
 
-runExpr :: Bool -> Maybe FilePath -> String -> IO ()
-runExpr dbg mfile stmt = do
-  functions <-
-    case mfile of
-      Nothing -> do
-        cfg <- getAppUserDataDirectory "hwk.hs"
-        ifM (doesFileExist cfg) (lines <$> readFile cfg) (return [])
-      Just f -> lines <$> readFile f
-  withTempDir $ \tempDir -> do
-    let tempFile = tempDir </> "hwk.hs"
-    writeFile tempFile $ genScript functions stmt
-    if dbg then do
-      ok <- cmdBool "runghc" [tempFile]
-      unless ok $ readFile tempFile >>= error'
-      else void $ rawSystem "runghc" [tempFile]
-
-genScript :: [String] -> [Char] -> String
-genScript functions stmt =
-  unlines $ [extensions, imports] ++ functions ++ genFunction "hwk" stmt : genMain "hwk" : [toInt, toList]
+runExpr :: Bool -> Maybe FilePath -> String -> [FilePath] -> IO ()
+runExpr _all mfile stmt files = do
+  mapM_ checkFileExists files
+  input <- lines <$> getContents
+  usercfg <- getAppUserDataDirectory "hwk.hs"
+  unlessM (doesFileExist usercfg) $ do
+    datadir <- getDataDir
+    copyFile (datadir </> "hwk.hs") usercfg
+  -- FIXME check file exists
+  let mods = usercfg : maybeToList mfile
+  r <- runInterpreter (runHint mods input)
+  case r of
+    Left err -> putStrLn $ errorString err
+    Right () -> return ()
   where
-    extensions, imports :: String
-    extensions = "{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}"
+    runHint :: [FilePath] -> [String] -> Interpreter ()
+    runHint mods input = do
+      loadModules mods
+      setImports ["Prelude", "Data.List", "Data.Char", "Hwk"]
+      -- -- Not sure which is better: typechecking manually or polymorph string
+      -- etypchk <- typeChecksWithDetails stmt
+      -- case etypchk of
+      --   Left err -> error' err
+      --   Right typ ->
+      --     case typ of
+      poly <- do
+        havePoly <- typeChecks "polymorph"
+        if havePoly then do
+          s <- interpret "polymorph" infer
+          return $ if null s then "" else s ++ " . "
+          else return ""
+      fn <- interpret (poly ++ stmt) (as :: [String] -> [String])
+      liftIO $ mapM_ putStrLn (fn input)
 
-    imports = unlines [ "import Data.List"
-                      , "import Data.List.Split"
-                      , "import Data.Char"
-                      ]
+    checkFileExists :: FilePath -> IO ()
+    checkFileExists file = do
+      unlessM (doesFileExist file) $
+        error' $ "file not found: " ++ file
 
-    genMain :: String -> String
-    genMain fname =
-      "main = getContents >>= mapM_ putStrLn . toList . " ++ fname ++ " . lines"
-
-    toList, toInt :: String
-    toList = unlines [ "class ToList a where toList :: a -> [String]"
-                     , "instance ToList Integer where toList x = [show x]"
-                     , "instance ToList String where toList x = [x]"
-                     , "instance ToList [Integer] where toList lst = map (\\x -> show x) lst"
-                     , "instance ToList [String] where toList = id"
-                     , "instance ToList [[String]] where toList = map (intercalate \"\\t\")"
-                     ]
-
-    toInt = unlines [ "int str = read str :: Integer"
-                    , "ints = map int"
-                    ]
-
-genFunction :: [Char] -> [Char] -> [Char]
-genFunction fname stmt = fname ++ " = " ++ stmt
+    errorString :: InterpreterError -> String
+    errorString (WontCompile es) = L.intercalate "\n" (header : map unbox es)
+      where
+        header = "ERROR: Won't compile:"
+        unbox (GhcError e) = e
+    errorString e = show e
---
+++ data/hwk.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Hwk where
+
+import Data.List (intercalate)
+
+-- this string is used by hwk
+-- | Determines the types of functions hwk can interpret
+-- "toList" allows some simple polymorphism
+-- use "id" or "" to allow only functions of type: [String] -> [String]
+polymorph :: String
+polymorph = "toList"
+
+-- ToList allows handling functions of type: ToList a => [String] -> a
+class ToList a where
+  toList :: a -> [String]
+instance ToList String where
+  toList x = [x]
+instance ToList [String] where
+  toList = id
+instance ToList [[String]] where
+  toList = map (intercalate "\t")
+instance ToList Integer where
+  toList x = [show x]
+instance ToList [Integer] where
+  toList lst = map show lst
+
+-- below here all user defined ------
+
+int :: String -> Integer
+int str = read str :: Integer
+
+ints :: [String] -> [Integer]
+ints = map int
