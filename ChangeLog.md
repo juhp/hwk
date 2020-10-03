a1d84d871186f23154711d3af04ac3b4c8addeb7
Jens Petersen <petersen@redhat.com>
2020-10-03 17:56:34 +0800

    initial reworking type sigs, drop envvars and use more libs

---
+++ ChangeLog.md
@@ -0,0 +1,8 @@
+# Revision history for hwk
+
+## 0.2
+- first release by Jens Petersen
+
+## 0.1.0.0 (2016-2017)
+- original project by Lukas Martinelli
+  https://github.com/lukasmartinelli/hwk
--- Main.hs
+++ Main.hs
@@ -1,68 +1,63 @@
-module Main where
+import Control.Monad.Extra
+import SimpleCmd
+import SimpleCmdArgs
+import System.Directory
+import System.FilePath
+import System.IO.Extra
+import System.Process
 
-import System.Environment (getArgs, getEnvironment)
-import System.Exit (exitSuccess, exitWith, ExitCode(..))
-import System.Posix.Process (executeFile)
-import System.Posix.Temp (mkdtemp)
-import Data.List (isPrefixOf)
+import Paths_hwk (version)
 
 main :: IO ()
-main = getArgs >>= parse
+main =
+  simpleCmdArgs (Just version) "A Haskell awk/sed like tool"
+    "Simple shell text processing with Haskell" $
+  runExpr <$> switchWith 'd' "debug" "Output Haskell" <*> optional (strOptionWith 'f' "file" "FILE" "User function include file") <*> strArg "FUNCTION"
 
-parse [] = usage >> die
-parse ["-h"] = usage   >> exit
-parse ["-v"] = version >> exit
-parse ["clear"] = do
-    envs <- getEnvironment
-    putStrLn $ unlines $ envUnsetFunctions envs
-    exit
-parse ["env", fname, stmt] = do
-    putStrLn $ "export " ++ envFunctionPrefix ++ fname ++ "='" ++ stmt ++ "'"
-    exit
-parse [stmt] = do
-    envs <- getEnvironment
-    tempDir <- mkdtemp "/tmp/"
-    let tempFile = tempDir ++ "/hwk.hs"
-    writeFile tempFile $ genModule (envFunctions envs) stmt
-    executeFile "runhaskell" True [tempFile] Nothing
+runExpr :: Bool -> Maybe FilePath -> String -> IO ()
+runExpr dbg mfile stmt = do
+  functions <- do
+    case mfile of
+      Nothing -> do
+        cfg <- getAppUserDataDirectory "hwk.hs"
+        ifM (doesFileExist cfg) (lines <$> readFile cfg) (return [])
+      Just f -> lines <$> readFile f
+  withTempDir $ \tempDir -> do
+    let tempFile = tempDir </> "hwk.hs"
+    writeFile tempFile $ genScript functions stmt
+    if dbg then do
+      ok <- cmdBool "runghc" [tempFile]
+      unless ok $ readFile tempFile >>= error'
+      else void $ rawSystem "runghc" [tempFile]
 
-extensions = "{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}"
-imports = unlines [ "import Data.List.Split"
-                  , "import Data.List"
-                  ]
-genMain fname = "main = getContents >>= \\contents -> " ++
-                "mapM_ putStrLn $ toList $ " ++ fname ++ " $ lines contents"
+genScript :: [String] -> [Char] -> String
+genScript functions stmt =
+  unlines $ [extensions, imports] ++ functions ++ genFunction "hwk" stmt : genMain "hwk" : [toInt, toList]
+  where
+    extensions, imports :: String
+    extensions = "{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}"
 
-toList = unlines [ "class ToList a where toList :: a -> [String]"
-                 , "instance ToList Integer where toList x = [show x]"
-                 , "instance ToList String where toList x = [x]"
-                 , "instance ToList [Integer] where toList lst = map (\\x -> show x) lst"
-                 , "instance ToList [String] where toList = id"
-                 , "instance ToList [[String]] where toList = map (intercalate \"\\t\")"
-                 ]
+    imports = unlines [ "import Data.List"
+                      , "import Data.List.Split"
+                      , "import Data.Char"
+                      ]
 
-toInt = unlines [ "int str = read str :: Integer"
-                , "ints = map int"
-                ]
+    genMain :: String -> String
+    genMain fname =
+      "main = getContents >>= mapM_ putStrLn . toList . " ++ fname ++ " . lines"
 
-libraryFunctions = [toInt, toList]
+    toList, toInt :: String
+    toList = unlines [ "class ToList a where toList :: a -> [String]"
+                     , "instance ToList Integer where toList x = [show x]"
+                     , "instance ToList String where toList x = [x]"
+                     , "instance ToList [Integer] where toList lst = map (\\x -> show x) lst"
+                     , "instance ToList [String] where toList = id"
+                     , "instance ToList [[String]] where toList = map (intercalate \"\\t\")"
+                     ]
+
+    toInt = unlines [ "int str = read str :: Integer"
+                    , "ints = map int"
+                    ]
+
+genFunction :: [Char] -> [Char] -> [Char]
 genFunction fname stmt = fname ++ " = " ++ stmt
-genModule functions stmt = unlines $ [extensions, imports, genFunction "hwk" stmt, genMain "hwk"] ++
-                           functions ++  libraryFunctions
-
-exit    = exitSuccess
-die     = exitWith (ExitFailure 1)
-envFunctions envs = [envGenFunction e | e <- envs, envFunctionPrefix `isPrefixOf` fst e]
-envGenFunction (fname, stmt) = genFunction (drop (length envFunctionPrefix) fname) stmt
-envUnsetFunctions envs = ["unset " ++ fst e | e <- envs, envFunctionPrefix `isPrefixOf` fst e]
-envFunctionPrefix = "HWK_FUNCTION_"
-version = putStrLn "hwk - Haskell based AWK replacement v0.1"
-usage   = putStr $ unlines [ "Usage:"
-                           , "  hwk env <fname> <stmt>"
-                           , "  hwk clear"
-                           , "  hwk <stmt>"
-                           , ""
-                           , "Options:"
-                           , "  -h   Show help screen."
-                           , "  -v   Show version."
-                           ]
