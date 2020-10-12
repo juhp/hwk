87529f33413230b0e4c0b72bad146daa0a9f6baf
Jens Petersen <petersen@redhat.com>
2020-10-13 02:34:10 +0800

    Hwk: add Data.Version.Extra

--- Hwk.hs
+++ Hwk.hs
@@ -3,8 +3,8 @@ module Hwk where
 -- | modules to be imported in hint interpreter
 -- These modules must be from installed packages
 userModules :: [String]
-userModules = [ "Prelude", "Data.List", "Data.Char", "Data.Bool"
-              , "Data.List.Extra", "Data.Tuple.Extra"
+userModules = [ "Prelude", "Data.Bool", "Data.Char", "Data.List"
+              , "Data.List.Extra", "Data.Tuple.Extra", "Data.Version.Extra"
               , "System.FilePath"
               ]
 
